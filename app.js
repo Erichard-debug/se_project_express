d16f8adf883bade2c30edf10a8d8fd58a16a2401
Erichard <erichard660@gmail.com>
2023-11-19 21:15:27 -0800

    wtwr fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
 const app = express();
 mongoose.connect(
-  "mongodb://127.0.0.1:27017/wrwt_db",
+  "mongodb://127.0.0.1:27017/wtwr_db",
   (r) => {
     console.log("Connected to DB", r);
   },
